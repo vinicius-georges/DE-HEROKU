368358661fd0e4b99f29bad61fa4f9ac3c759513
leofyada <42298873+leofyada@users.noreply.github.com>
2021-04-15 10:53:00 -0300

    Update PROGRAMA_DE.py

--- PROGRAMA_DE.py
+++ PROGRAMA_DE.py
@@ -2,7 +2,7 @@
 import base64
 import streamlit as st
 import pandas as pd
-from zipfile import ZipFile
+from zipfile import ZipFile 
 
 # Criando um título para o aplicativo
 st.title("Aplicativo para dividir dados por DE")
