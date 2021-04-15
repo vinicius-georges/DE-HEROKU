8363e0c9d417571de8c4f105924e6ec10ceeb02d
leofyada <42298873+leofyada@users.noreply.github.com>
2021-04-15 11:15:29 -0300

    Update PROGRAMA_DE.py

--- PROGRAMA_DE.py
+++ PROGRAMA_DE.py
@@ -11,10 +11,10 @@ st.text("Faça o upload do arquivo. "
         "\n\nIMPORTANTE: a coluna com a DE deve ser a primeira da planilha.")
 # Atribuindo o arquivo a uma variável denominada "arquivo"
 arquivo = st.file_uploader("Upload do arquivo", type=["csv", "xlsx"])
-# Realizando a leitura do arquivo excel e atribuindo à variável "arquivo_lido"
-arquivo_lido = pd.read_excel(arquivo)
-# Renomeando a primeira coluna para "NM_DIRETORIA"
-if arquivo_lido is not None:
+if arquivo is not None:
+    # Realizando a leitura do arquivo excel e atribuindo à variável "arquivo_lido"
+    arquivo_lido = pd.read_excel(arquivo)
+    # Renomeando a primeira coluna para "NM_DIRETORIA"
     arquivo_lido.rename(columns={list(arquivo_lido)[0]: "NM_DIRETORIA"}, inplace=True)
     # Criando um DataFrame com apenas as Diretorias de Ensino
     de = arquivo_lido.iloc[0:len(arquivo_lido), 0:1].drop_duplicates()
@@ -25,8 +25,8 @@ if arquivo_lido is not None:
     st.write("Todos os arquivos terminarão com o nome da DE.")
     name = st.text_input("Digite o início do nome dos arquivos. "
                          "\n\n Exemplo: se digitar 'DE-', o nome do arquivo da DE de ITU será DE-ITU.")
-    
-    
+
+
     # Criando um loop para separar os dados e criar arquivos
     for n in range(0, len(de), 1):
         base_de = arquivo_lido[arquivo_lido["NM_DIRETORIA"] == de.iloc[n, 0]]
@@ -36,8 +36,8 @@ if arquivo_lido is not None:
         zipObj.write(name+de.iloc[n, 0]+".xlsx")
     # fechando o arquivo Zip
     zipObj.close()
-    
-    
+
+
     with open(ZipfileDotZip, "rb") as f:
         bytes = f.read()
         b64 = base64.b64encode(bytes).decode()
